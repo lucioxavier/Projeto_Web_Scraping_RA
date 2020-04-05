118408d3506b694fa8f8a278af148a079bee4d38
Lucio Xavier <lucioflaviowe@hotmail.com>
2020-04-05 03:10:55 -0300

    Update script_RA.py

--- script_RA.py
+++ script_RA.py
@@ -5,10 +5,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC # Importar classe para ajudar a localizar os elementos
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
+problems_links = []
+problems_links_2 = []
 links = []
-l = []
 
+#Definição - Funções de estilização
 def titulo_in():
     titulo_ = 'WEBSCRAPING - RECLAME AQUI'
     print('='*40)
@@ -18,56 +21,105 @@ def titulo_in():
 def linha_div():
     print('-'*40)
 
+#Definição - Função de interação com o usuário
 def def_operadora():
+    global pag_in, qtd_pag, operadora, url_in, url_fin
     linha_div()
+    pag_in = int(input('Digite a página que deseja iniciar: '))
+    qtd_pag = int(input('Digite a quantidade de páginas desejadas: '))
     operadora = str(input('Digite a operadora que deseja pesquisar: ')).upper().strip()
-    global url_in
     url_in = f'https://www.reclameaqui.com.br'
     url_tim = f'/empresa/tim-celular/lista-reclamacoes/?pagina='
     url_vivo = f'/empresa/vivo-celular-fixo-internet-tv/lista-reclamacoes/?pagina='
     url_claro = f'/empresa/claro/lista-reclamacoes/?pagina='
     url_oi = f'/empresa/oi-movel-fixo-tv/lista-reclamacoes/?pagina='
-    global url_fin
     if operadora in 'TIM':
+        linha_div()
         url_fin = f'{url_in}{url_tim}'
     elif operadora in 'VIVO':
+        linha_div()
         url_fin = f'{url_in}{url_vivo}'
+        linha_div()
     elif operadora in 'CLARO':
+        linha_div()
         url_fin = f'{url_in}{url_claro}'
     elif operadora in 'OI':
+        linha_div()
         url_fin = f'{url_in}{url_oi}'
     else:
-        print(f'Você não selecionou uma operadora elegível!')
+        linha_div()
+        print(f'Você não selecionou uma operadora elegível!\nExecute o código novamente.')
 
+#Defnição - Funções de execução
 def abrir_navegador():
     global chrome, wait
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors-spki-list')
     options.add_argument('--ignore-ssl-errors')
     chrome = webdriver.Chrome(executable_path='C:\PYTHON\chromedriver', chrome_options=options)
-    wait = WebDriverWait(chrome, 1)
+    wait = WebDriverWait(chrome, 4)
+    linha_div()
     print('Navegador aberto...')
 
 def carregando_url(url_):
-    chrome.get(url_)
-    print('Carregando a página...')
-    sleep(4)
+    global ra_page
+    button_path = '//*[@id="brand-page-controller"]/div[4]/div[2]/div[2]/div[2]/div/div[3]/div[3]/div[3]/ul/li[8]/a'
+    elemento = False
+    contador = 0
+    while elemento is False:
+        try:
+            chrome.get(url_)
+            chrome.implicitly_wait(4)
+            elemento = wait.until(EC.element_to_be_clickable((By.XPATH, button_path)))
+        except:
+            contador +=33
+            sleep(2)
+            print(f'Tentando carregar página novamente... {contador}%')
+            if contador > 100:
+                problems_links.append(url_)
+                print('Página não carregada... Link adicionado aos problems_links.')
+                break
+        else:
+            print('Página carregada com sucesso!')
+            ra_page = bs(chrome.page_source, 'html.parser')
+            sleep(4)
+            elemento = True
+
+def carregando_url_2(url_2):
+    global ra_page_2
+    button_path_2 = '//*[@id="complain-detail"]/div/div[2]/div/div[1]'
+    elemento_2 = False
+    contador_2 = 0
+    while elemento_2 is False:
+        try:
+            chrome.get(url_2)
+            chrome.implicitly_wait(4)
+            elemento_2 = wait.until(EC.element_to_be_clickable((By.XPATH, button_path_2)))
+        except:
+            contador_2 +=33
+            sleep(2)
+            print(f'Tentando carregar página novamente... {contador_2}%')
+            if contador_2 > 100:
+                problems_links_2.append(url_2)
+                print('Página não carregada... Link adicionado aos problems_links.')
+                break
+        else:
+            print('Página carregada com sucesso!') 
+            ra_page_2 = bs(chrome.page_source, 'html.parser')
+            sleep(4)
+            elemento_2 = True
 
 def criando_arq():
-    with open('reclame_aqui.csv', 'w') as _file:
+    with open(f'reclame_aqui_{operadora}.csv', 'w') as _file:
         _file.write('Titulo_reclamacao; Empresa; Local; ID_reclamacao; Data; Hora; Descricao\n') 
 
 titulo_in()
-qtd_pag = int(input('Digite a quantidade de páginas desejadas: '))
-linha_div()
-criando_arq()
 def_operadora()
+criando_arq()
 abrir_navegador()
 
-for c in range(1, qtd_pag+1):
-    carregando_url(f'{url_fin}{c}')
-    ra_page = bs(chrome.page_source, 'html.parser')
-    sleep(4)
+for page in range(pag_in, qtd_pag+1):
+    carregando_url(f'{url_fin}{page}')
     boxes = ra_page.find('div', {'class': 'content-loader'})
     sleep(4)
 
@@ -76,42 +128,25 @@ for c in range(1, qtd_pag+1):
         print(f'Salvando link ({c+1}/{len(boxes)})')
         sleep(2)
 
-    print('acessando links')
+    linha_div()
+    print('Iniciando processo de acesso aos links...')
+    linha_div()
     for link in links:
-        recarregar = False
-        contador = 0
-        while recarregar is False:
-            try:
-                carregando_url(f'{url_in}{link}')
-                ra_page2 = bs(chrome.page_source, 'html.parser')
-                selection_1 = ra_page2.find('div', {'class': 'col-md-10 col-sm-12'})
-                selection_2 = selection_1.find('ul')
-            except:
-                contador += 3
-                sleep(3)
-                print(f'Tentando novamente... {contador}')
-                if contador > 100:
-                    break
-                else:
-                    print(f'Aguarde...{contador}')
-                    sleep(2)
-            else:
-                print('Página carregada com sucesso...')
-                break
-        
+        carregando_url_2(f'{url_in}{link}')
+        selection_1 = ra_page_2.find('div', {'class': 'col-md-10 col-sm-12'})
+        selection_2 = selection_1.find('ul')
         titulo = selection_1.find('h1').text.strip()
         empresa = selection_1.find('p').text.strip()
         local = selection_2.find('li', {'class':'ng-binding'}).text.strip()
         id_ = selection_2.find('li', {'class':'ng-scope'}).text.strip()
         data = selection_2.find_all('li', {'class':'ng-binding'})[1].text[0:9].strip()
         hora = selection_2.find_all('li', {'class':'ng-binding'})[1].text[-5:].strip()
-        descricao = ra_page2.find('div', {'class':'complain-body'}).text.strip()
+        descricao = ra_page_2.find('div', {'class':'complain-body'}).text.strip()
         l = [titulo, empresa, local, id_, data, hora, descricao]
-        with open('reclame_aqui.csv', 'a') as _file2:
+        with open(f'reclame_aqui.{operadora}csv', 'a') as _file2:
             _file2.write(f'{l[0]};{l[1]};{l[2]};{l[3]};{l[4]};{l[5]};{l[6]}\n')
         l.clear()
         sleep(4)
-        contador = 0
     links.clear()
     print(f'Todos links deste bloco carregados.')
 
